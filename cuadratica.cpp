58efe0b78e6531cb73fbf90e3e6b60e4112d7d83
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-10-15 07:53:33 -0600

    Formula general

---
+++ cuadratica.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int a, b ,c, x1, x2;
+double r;
+cout<<"Ingresa los valores de A, B y C"<<endl;
+cin>>a>>b>>c;
+r=sqrt(pow(b,2)-4*a*c);
+x1=(-b+r)/(2*a);
+x2=(-b-r)/(2*a);
+if(a == 0 or ((pow(b,2)-4*a*c)<0))
+   {
+       cout<<"La raiz es negativa o no se puede resolver este problema";
+        return 0;
+   }
+cout<<"las soluciones son: X1= "<<x1<<" y X2= "<<x2;
+
+
+
+
+
+
+return 0;
+}
